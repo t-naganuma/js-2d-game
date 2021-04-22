f1fb6dc5f33cc121003393ef93848fc841aa2df7
t-naganuma <takuya.naganuma@gmail.com>
2021-04-22 18:08:28 +0900

    cancel button追加

--- public/js/stageObject.js
+++ public/js/stageObject.js
@@ -131,8 +131,8 @@ let stage;
 async function getStageJson() {
     // ステージ情報を取得
     // console.log(process.env.BASE_URL);
-    // await axios.get('http://localhost:5000/gameInfo.json')
-    await axios.get('https://js-2d-game.herokuapp.com/gameInfo.json')
+    await axios.get('http://localhost:5000/gameInfo.json')
+    // await axios.get('https://js-2d-game.herokuapp.com/gameInfo.json')
         .then(response => {
             stage = new StageObject(response.data);
             stage.setStage();
